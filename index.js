ca2c26e18a1f51aa5bcf58900519aa3a9cff0222
TechTutz <95581907+TechTutz@users.noreply.github.com>
2022-10-15 16:59:55 -0500

    get stuff in repo

---
+++ index.js
@@ -0,0 +1,24 @@
+// Number
+const myNumber = 99;
+
+// string
+const myString = 'hello!';
+
+// boolean
+const myBool = true;
+
+// undefined
+let myUndefined;
+
+// null
+const myNull = null;
+
+// object
+const myObject = { name: 'Bob', age: 42 };
+
+// bigInt
+const myBigInt = 33322211122233n;
+
+// symbol
+const mySymbol = Symbol('hello');
+const mySymbol2 = Symbol('hello');
